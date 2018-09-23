c373183708add134909233ad42045a420918b631
skxeve <skxeve@gmail.com>
2018-09-23 15:44:24 +0900

    add packages Galaxy S8.

--- show-packages.sh
+++ show-packages.sh
@@ -4,6 +4,8 @@ opt=$1
 
 if [ `echo $opt |grep "d"` ]; then
     adb shell cmd package list packages|awk -F':' '{print $2}'|sort| egrep 'docomo|dcm'
+elif [ `echo $opt |grep "samsung"` ]; then
+    adb shell cmd package list packages|awk -F':' '{print $2}'|sort| egrep 'samsung'
 elif [ `echo $opt |grep "s"` ]; then
     adb shell cmd package list packages|awk -F':' '{print $2}'|sort| egrep 'sony'
 else
